e140bd31983c7cafad12a942a34d60b648f6c64a
Abdulwaarith Zakariyya <abdulwaarithz@gmail.com>
2022-08-24 14:21:23 +0100

    First commit

---
+++ src/api.js
@@ -0,0 +1,24 @@
+ export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2881519063mshb89f6148ae5e732p1c95b7jsn776fd4b0fc21',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '2881519063mshb89f6148ae5e732p1c95b7jsn776fd4b0fc21',
+// 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
